50460d1558629d7c5a9eba13d499dc1deb0aac88
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2018-06-07 19:02:28 -0300

    primeiro commit

---
+++ hello.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# nome = input('Digite seu nome: ')
+
+# print('Seja bem vindo!', nome.lower())
+# print('Seja bem vindo!', nome.title())
+# print('Seja bem vindo!', nome.upper())
+
+# num1 = int(input('Digite o primeiro numero: '))
+# num2 = int(input('Digite o segundo numero:  '))
+# print('O primeiro numero: {}. O segundo numero: {}'.format(num1, num2))
+
+# nome = str(input('Digite o nome do aluno: '))
+# nota1 = int(input('Digite a primeira nota: '))
+# nota2 = int(input('Digite a segunda nota: '))
+# media = (nota1 + nota2) / 2
+# print('A nota media do aluno {} é {}'.format(nome, media))
+
+# nomes = ['laranja', 'goiaba', 'tomate', 'banana', 'morango']
+# print(nomes[3])
+
+# letras = ['a', 'b', 'c']
+# letras.append('d')
+# print(letras)
+
+
+
+
+
+
